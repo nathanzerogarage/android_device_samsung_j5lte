bee6402e9f9843c16c69a59d7f54d8683468beab
Nathan Andrew <nathannxx@proton.me>
2025-06-07 06:50:52 +0700

    j5lte: qassa-ify
    Signed-off-by: Nathan Andrew <nathannxx@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,8 @@
 LOCAL_PATH := device/samsung/j5lte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_j5lte.mk \
-	$(LOCAL_DIR)/rr_j5lte.mk \
-	$(LOCAL_DIR)/pa_j5lte.mk \
-	$(LOCAL_DIR)/lineage_j5lte.mk
+	$(LOCAL_DIR)/qassa_j5lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_j5lte-eng \
-    lineage_j5lte-userdebug \
-	lineage_j5lte-user
+    qassa_j5lte-eng \
+    qassa_j5lte-userdebug \
+    qassa_j5lte-user
